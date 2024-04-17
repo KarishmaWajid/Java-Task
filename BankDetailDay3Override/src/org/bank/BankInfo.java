0ca28d012a6def6d0babf4827a6e00e2bfc21e9f
lenovo <lenovo@DESKTOP-9TSK6Q7>
2024-04-17 19:54:27 +0530

    java Task completed

---
+++ BankDetailDay3Override/src/org/bank/BankInfo.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("Saving: 2.5%");
+
+	}
+	public void fixed() {
+		System.out.println("Fixed: 3% ");
+
+	}
+	public void deposit() {
+		System.out.println("Deposit : 4%");
+
+	}
+
+}
